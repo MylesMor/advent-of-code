cbf50eeabf73c2697a9d49ad81a9ad51a0f53f24
Myles <mylesmor@gmail.com>
2020-12-01 19:16:58 +0000

    Update Dec1.py

--- Dec1/Dec1.py
+++ Dec1/Dec1.py
@@ -1,7 +1,6 @@
 import itertools
 import functools
 import operator
-import time
 
 def read_file(filename):
     '''Returns a list of integers from a file.'''
@@ -9,24 +8,12 @@ def read_file(filename):
         numbers = [int(line) for line in file]
     return numbers
 
-
 def report_repair(numbers, n, value):
-    '''
-    Finds n numbers in a list that sum to value.
-
-    Parameters:
-        numbers: A list of numbers.
-        n: The number of numbers to be summed together to find value.
-        value: The value the summed numbers should equal.
-
-    Returns:
-        The product of the n numbers that sum to value.
-    '''
-
+    '''Finds n numbers in a list that sum to value.'''
     for combination in itertools.combinations(numbers, n):
         if sum(combination) == value:
             print("Answer: " + str(functools.reduce(operator.mul, combination)))
-            return
+            return 
 
 if __name__ == "__main__":
     numbers = read_file("input.txt")
