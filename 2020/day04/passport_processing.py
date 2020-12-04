15b9f2765d3fd8ce0f8bdf3c068657c03799a340
Myles <mylesmor@gmail.com>
2020-12-04 15:00:59 +0000

    Add day04 solution

---
+++ 2020/day04/passport_processing.py
@@ -0,0 +1,71 @@
+import re
+
+def read_file(filename):
+    '''Returns a list of passport dictionaries.'''
+    with open(filename, "r") as file:
+        lines = file.readlines()
+        lines.append("\n")
+        passport_list = []
+        one_passport = ""
+        for line in lines:
+            if line != "\n":
+                one_passport += line.replace("\n", " ")
+                continue
+            data_fields = one_passport[:-1].split(" ")
+            passport_dict = {}
+            for data in data_fields:
+                data_split = data.split(":")
+                passport_dict[data_split[0]] = data_split[1]
+            passport_list.append(passport_dict)
+            one_passport = ""
+    return passport_list
+    
+
+def passport_processing(passports, validate_data):
+    valid_passports = 0
+    for passport in passports:
+        if check_data(passport, validate_data):
+            valid_passports += 1
+    return valid_passports
+
+
+def check_data(passport, validate_data):
+    '''Validates a passport.
+
+    Validates a invidivual passport - ensures the required fields are present and 
+    optionally validates the data.
+
+    Args:
+        passport (dict): The passport to validate.
+        validate_data (bool): True to validate the data, otherwise False.
+
+    Returns:
+        bool: True if valid passport, otherwise False.
+    
+    '''
+    if len(passport) < 7:
+        return False
+    if len(passport) < 8 and "cid" in passport:
+        return False
+    if validate_data:
+        if not re.match("19[2-9][0-9]|200[0-2]", passport['byr']):
+            return False
+        if not re.match("201[0-9]|2020", passport['iyr']):
+            return False
+        if not re.match("202[0-9]|2030", passport['eyr']):
+            return False
+        if not re.match("1[5-8][0-9]cm|19[0-3]cm|6[0-9]in|59in|7[0-6]in", passport['hgt']):
+            return False
+        if not re.match("#[0-9a-f]{6,6}$", passport['hcl']):
+            return False
+        if not re.match("amb|blu|brn|gry|grn|hzl|oth", passport['ecl']):
+            return False
+        if not re.match("^[0-9]{9,9}$", passport['pid']):
+            return False 
+    return True
+
+
+if __name__ == "__main__":
+    passports = read_file("input.txt")
+    print("Challenge 1 Answer: " + str(passport_processing(passports, False)))
+    print("Challenge 2 Answer: " + str(passport_processing(passports, True)))
