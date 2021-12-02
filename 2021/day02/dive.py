3d95c599bf7e4f746587806d9373c25d312a0ae6
Myles <mylesmor@gmail.com>
2021-12-02 11:20:35 +0000

    Add day02

---
+++ 2021/day02/dive.py
@@ -0,0 +1,43 @@
+import timeit 
+
+def read_file(input):
+    '''Returns a list of integers from a file.'''
+    data = []
+    with open(input, "r") as file:
+        for line in file:
+            split_line = line.split(" ")
+            if (split_line[0] == "up"):
+                data.append({"depth": -int(split_line[1])})
+            elif (split_line[0] == "down"):
+                data.append({"depth": int(split_line[1])})
+            elif (split_line[0] == "forward"):
+                data.append({"forward": int(split_line[1])})
+    return data
+
+
+def calculate_final_position(data):
+    position = {"forward": 0, "depth": 0}
+    for instruction in data:
+        for k, v in instruction.items():
+            position[k] += v
+    return position['forward'] * position['depth']
+
+
+def calculate_final_position_with_aim(data):
+    position = {"forward": 0, "depth": 0, "aim": 0}
+    for instruction in data:
+        for k, v in instruction.items():
+            if k == "forward":
+                position[k] += v
+                position['depth'] += position['aim'] * v
+            else:
+                position['aim'] += v
+    return position['forward'] * position['depth']
+
+
+if __name__ == "__main__":
+    start_time = timeit.default_timer()
+    data = read_file("input.txt")
+    print(f'Challenge 1 Answer: {calculate_final_position(data)}')
+    print(f'Challenge 2 Answer: {calculate_final_position_with_aim(data)}')
+    print("Time taken: %s" % (round(timeit.default_timer() - start_time, 8)))
